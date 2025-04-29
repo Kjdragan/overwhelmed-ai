11c9f00c4ceca954ca5d7f28d6103f5803dd36ec
Kevin Dragan <kevin.dragan@outlook.com>
2025-04-29 10:29:24 -0500

    feat(module/yt_ingest): declare project_number var

--- modules/yt_ingest/variables.tf
+++ modules/yt_ingest/variables.tf
@@ -1,2 +1,12 @@
-variable "project_id"    { type = string }
-variable "func_sa_email" { type = string }
+variable "project_id" {
+  type = string
+}
+
+variable "project_number" {
+  description = "Numeric GCP project number for default compute SA"
+  type        = number
+}
+
+variable "func_sa_email" {
+  type = string
+}
